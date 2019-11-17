6a9d5d6fa8d3b8e338ff8e914b4bdd6e06803155
Jeff Helsten <Jeffh@jane.com>
2019-11-17 00:57:49 -0700

    init

---
+++ Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+	public static class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new MvcMovieContext(
+				serviceProvider.GetRequiredService<
+					DbContextOptions<MvcMovieContext>>()))
+			{
+				// Look for any movies.
+				if (context.Movie.Any())
+				{
+					return;   // DB has been seeded
+				}
+
+				context.Movie.AddRange(
+					new Movie
+					{
+						Title = "The RM",
+						ReleaseDate = DateTime.Parse("1989-2-12"),
+						Genre = "RoMantic Comedy",
+						Price = 7.99M,
+                        Rating = "G"
+					},
+
+					new Movie
+					{
+						Title = "Other Side of Heaven ",
+						ReleaseDate = DateTime.Parse("1984-3-13"),
+						Genre = "Comedy",
+						Price = 8.99M,
+						Rating = "G"
+					},
+
+					new Movie
+					{
+						Title = "Charly",
+						ReleaseDate = DateTime.Parse("1986-2-23"),
+						Genre = "Comedy",
+						Price = 9.99M,
+						Rating = "G"
+					},
+
+					new Movie
+					{
+						Title = "Meet the Mormons",
+						ReleaseDate = DateTime.Parse("1959-4-15"),
+						Genre = "Western",
+						Price = 3.99M,
+						Rating = "G"
+					}
+				);
+				context.SaveChanges();
+			}
+		}
+	}
+}
